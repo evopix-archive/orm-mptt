461a5dc85b25d463338fa5a121255042792d4c07
Brandon Summers <brandon@evolutionpixels.com>
2010-03-08 12:04:39 -0800

    initial commit

---
+++ config/mptt.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'left_column' => 'lft',
+	'right_column' => 'rgt',
+	'level_column' => 'lvl',
+	'scope_column' => 'scope',
+	'parent_column' => 'parent_id'
+);
