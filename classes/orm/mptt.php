0a350fb6e02d9610d24cd575f3445cf93bea40a6
Heath Padrick <hpadrick@comservecorp.com>
2010-07-13 21:53:34 +0800

    Corrected move() method to allow for moving an entire family

--- classes/orm/mptt.php
+++ classes/orm/mptt.php
@@ -531,19 +531,17 @@ class ORM_MPTT extends ORM {
 			$left_offset = ($left_column === TRUE ? $target->left() : $target->right()) + $left_offset;
 			$level_offset = $target->level() - $this->level() + $level_offset;
 			$size = $this->size();
-			$offset = ($left_offset - $this->left());
-			
-			$new_left = ($this->left() + $offset);
-			$new_right = ($this->right() + $offset);
-			$new_level = ($this->level() + $level_offset);
 			
 			$this->create_space($left_offset, $size);
 
 			$this->reload();
+
+			$offset = ($left_offset - $this->left());
 			
 			$this->_db->query(NULL, 'UPDATE '.$this->_table_name.'
-				SET `'.$this->left_column.'` = '.$new_left.', `'.$this->right_column.'` = '.$new_right.'
-				, `'.$this->level_column.'` = '.$new_level.'
+				SET `'.$this->left_column.'` = `'.$this->left_column.'` + '.$offset.'
+        , `'.$this->right_column.'` =  `'.$this->right_column.'` + '.$offset.'
+				, `'.$this->level_column.'` =  `'.$this->level_column.'` + '.$level_offset.'
 				, `'.$this->scope_column.'` = '.$target->scope().'
 				WHERE `'.$this->left_column.'` >= '.$this->left().'
 				AND `'.$this->right_column.'` <= '.$this->right().'
